28ec0745a3aff863efd5139d401d344327163bde
YoussefElharouch <elharouchyoussef1998@gmail.com>
2023-12-31 13:48:46 +0100

    Added a TextInput field in the searchWrapper view in Welcome.jsx

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import { useState } from 'react'
 import {
-   View,
-    Text,
-    TextInput,
-    TouchableOpacity,
-    Image,
-    FlatList,
-  
-  } from 'react-native'
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList,
 
+} from 'react-native'
+import { useRouter } from 'expo-router'
+import { icons, SIZES } from '../../../constants'
 
 
 
@@ -18,7 +19,33 @@ import styles from './welcome.style'
 const Welcome = () => {
   return (
     <View>
-      <Text>Welcome</Text>
+      {/* First View */}
+      <View
+        style={styles.container}
+      >
+        <Text
+          style={styles.userName}
+        >Welcome Youssef</Text>
+        <Text
+          style={styles.welcomeMessage}
+        >find your perfect job</Text>
+      </View>
+       {/* Second  View */}
+       <View
+       style={styles.searchContainer} >
+        
+        <View
+        style={styles.searchWrapper}
+        >
+          <TextInput
+          style={styles.searchInput}
+          
+          />
+        </View>
+       
+    
+       </View>
+
     </View>
   )
 }
